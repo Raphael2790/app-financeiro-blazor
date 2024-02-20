3b45122d082d1c83a1021d2dd2cddfd7a0f481d2
eduardoboca <eduardo.boca@gmail.com>
2024-02-20 18:38:15 -0300

    Inclusão dos serviços HttpClient e ApiCliente

--- src/AgioBank/Program.cs
+++ src/AgioBank/Program.cs
@@ -2,11 +2,15 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using AgioBank;
 using AgioBank.Services;
+using AgioBank.Clients.Interfaces;
+using AgioBank.Clients;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddSingleton<HttpClient>();
+builder.Services.AddScoped<IApiCliente, ApiCliente>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
         .AddScoped<IServicoConta, ServicoContaMock>();
         
