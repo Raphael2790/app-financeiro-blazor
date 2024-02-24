d098707338482372bae993b1a9c90593ae434aea
eduardoboca <eduardo.boca@gmail.com>
2024-02-24 18:40:00 -0300

    Finalizada arquitetura básica da aplicação

--- src/AgioBank/Pages/Contexts/AccountContext/UseCases/Create/Criar.razor.cs
+++ src/AgioBank/Pages/Contexts/AccountContext/UseCases/Create/Criar.razor.cs
@@ -1,8 +1,23 @@
-﻿using AgioBank.Pages.Contexts.SharedContext.ViewModels;
+﻿using AgioBank.Pages.Contexts.SharedContext.Enums;
+using AgioBank.Pages.Contexts.SharedContext.ViewModels;
 
 namespace AgioBank.Pages.Contexts.AccountContext.UseCases.Create
 {
     public class ViewModel : BaseViewModel
     {
+        public InputViewModel _model { get; set; } = new();
+        public EState PageState { get; set; } = EState.Idle;
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+        public async Task OnValidSubmitAsync()
+        {
+            PageState = EState.Busy;
+            await Task.Delay(2000);
+            PageState = EState.Success;
+            await Task.Delay(2000);
+            PageState = EState.Idle;
+        }
     }
 }
--- src/AgioBank/Pages/Contexts/AccountContext/UseCases/Create/InputViewModel.cs
+++ src/AgioBank/Pages/Contexts/AccountContext/UseCases/Create/InputViewModel.cs
@@ -1,10 +1,9 @@
-﻿using AgioBank.Contexts.AccountContext.Entities;
-using AgioBank.Pages.Contexts.SharedContext.Enums;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace AgioBank.Pages.Contexts.AccountContext.UseCases.Create
 {
-    public class InputModel
+    public class InputViewModel
     {
         [Required(ErrorMessage = "Informe o nome")]
         public string Nome { get; set; } = string.Empty;
@@ -12,7 +11,7 @@ namespace AgioBank.Pages.Contexts.AccountContext.UseCases.Create
         public string Telefone { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Informe o e-mail")]
-        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        [EmailAddress (ErrorMessage = "E-mail inválido")]
         public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Informe o número do documento")]
@@ -20,6 +19,14 @@ namespace AgioBank.Pages.Contexts.AccountContext.UseCases.Create
 
         [Required(ErrorMessage = "Informe o nome da sua mãe")]
         public string NomeMae { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Informe uma senha")]
+        public string Senha { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Confirme a senha")]
+        [Compare("Senha", ErrorMessage = "Senhas não conferem")]
+        public string ConfirmarSenha { get; set; } = string.Empty;
+
 
     }
 }
